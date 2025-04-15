69628b8f6069860d0fab1bbf1b3ce6f443d62ae5
Hassan Ahmed Khan <technicaladam0@gmail.com>
2025-04-15 17:07:30 +0500

    Footer design updated

--- src/components/Bloglist.jsx
+++ src/components/Bloglist.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import CommentSystem from './CommentSystem';
-import './BlogList.css';
+import './Bloglist.css';
 
 const Bloglist = () => {
   const [blogs, setBlogs] = useState([]);

70f49c0f65f8a20f72ec01f1183a42c5b8b35554
Hassan Ahmed Khan <technicaladam0@gmail.com>
2025-04-17 12:07:43 +0500

    new update

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -48,7 +48,7 @@ const AddBlog = ({ onBlogAdded }) => {
         if (onBlogAdded) {
           onBlogAdded();
         }
-        navigate('/');
+        navigate('/myblog');
       } else {
         setError(data.message || "Failed to post blog");
         alert(data.message || "Failed to post blog");
